5daab41eb0c54bd630c79c8018047852bdd40bc3
Chad Underhill <chad.j.underhill@gmail.com>
2017-10-02 17:37:10 -0400

    Initial structure added, data imported, image labels preprocessed

---
+++ template.py
@@ -0,0 +1,54 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+
+# Remove limits on numpy array print length
+np.set_printoptions(threshold=np.inf)
+
+# Load images in
+images = np.load('data/images.npy')
+
+# Load labels in
+labels = np.load('data/labels.npy')
+NUM_CLASSES = 10
+
+# Convert image labels to "one-hot vectors" of length ten
+one_hot_labels = keras.utils.to_categorical(labels, NUM_CLASSES)
+#for l in one_hot_labels:
+#    print(l)
+
+print(images)
+
+
+# Model Template
+
+model = Sequential() # declare model
+model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
+model.add(Activation('relu'))
+#
+#
+#
+# Fill in Model Here
+#
+#
+#model.add(Dense(10, kernel_initializer='he_normal')) # last layer
+#model.add(Activation('softmax'))
+
+
+# Compile Model
+#model.compile(optimizer='sgd',
+#              loss='categorical_crossentropy', 
+#              metrics=['accuracy'])
+
+# Train Model
+#history = model.fit(x_train, y_train, 
+#                    validation_data = (x_val, y_val), 
+#                    epochs=10, 
+#                    batch_size=512)
+
+
+# Report Results
+
+#print(history.history)
+#model.predict()
